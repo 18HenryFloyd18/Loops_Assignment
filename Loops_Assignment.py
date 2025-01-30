73e1a5101b54bb84e1ba1f6e65e3fe7d5e0ef42f
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-01-30 14:30:29 -0600

    Add files via upload

---
+++ Loops_Assignment.py
@@ -0,0 +1,11 @@
+numbersList = [1,1,2,3,5,8,13]
+numbersAdded = []
+while numbersList:
+    for each in numbersList:
+        print(each)
+    break
+    for each in numbersList:
+        print(0+1)
+
+ 
+    
